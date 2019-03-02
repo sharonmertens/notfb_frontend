224a571c5017be90748139ea27225d0d07ef38b6
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-01 20:02:26 -0500

    front end connected to backend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  fetchTasks = () => {
+    fetch('http://localhost:3000/users')
+    .then(data => data.json())
+    .then(jData => {
+      console.log(jData);
+    })
+  }
+
   render() {
     return (
       <div className="main-container">
-        <h1>!FB</h1>
+        <h1>!FB Test</h1>
+        <button onClick={this.fetchTasks}>Get Users</button>
       </div>
     );
   }
