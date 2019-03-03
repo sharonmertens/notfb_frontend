bf768a0e918f2eefbf6101953fef76a4e625be15
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-03-03 16:49:27 -0500

    handleChange function created for new post in NewPost.js

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react'
 
 class NewPost extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      keyed_item: ''
+    }
+  }
+
+  // handle change
+  handleChange = (e) => {
+    this.setState({ keyed_item: e.target.value })
+  }
+
+  // handle submit
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+    this.clearForm()
+  }
+
+  // clear the form
+  clearForm = () => {
+    this.setState({ keyed_item: '' })
+  }
+
   render () {
     return (
       <div className="new-post">
       this is the new post component
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="text"
+          value={this.props.keyed_item}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="image"
+          value={this.props.keyed_item}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="link"
+          value={this.props.keyed_item}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="author"
+          value={this.props.keyed_item}
+          onChange={this.handleChange}
+        />
+        <input type="submit" />
+      </form>
       </div>
     )
   }
