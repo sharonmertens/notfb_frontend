8fa5d1b9f7fb0adf91b5992ae2f940081ef874a8
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-05 04:35:06 -0500

    moved stuff from App.js to Dashboard.js due to auth changes, everything seems to work

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App extends Component {
     super(props)
     this.state = {
       // loggedIn: false,
-      text: '',
-      image: '',
-      link: '',
-      author: '',
-      posts: []
+      // text: '',
+      // image: '',
+      // link: '',
+      // author: '',
+      // posts: []
     }
   }
 
@@ -31,168 +31,131 @@ class App extends Component {
     })
   }
 
-  // fetch all posts
-  fetchPosts = () => {
-    fetch('http://localhost:3000/posts')
-    .then(data => data.json())
-    .then(jData => {
-      // console.log(jData)
-      this.setState({posts: jData})
-    })
-  }
-
-  // handles creating the post
-  handleCreatePost = (post) => {
-    console.log(this.state.posts)
-    fetch('http://localhost:3000/posts', {
-      body: JSON.stringify(post),
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(createdPost => {
-      // console.log(createdPost);
-      return createdPost.json()
-    })
-    .then(jData => {
-      this.updateArray(jData, 'posts')
-      console.log(jData)
-    })
-    .catch(err => console.log(err))
-  }
-
-  // handles updating the post
-  handleCheck = (post, arrayIndex) => {
-    // console.log(post)
-    // console.log(arrayIndex)
-    // manipulate the post data
-    this.editPost(post, arrayIndex)
-
-  }
-
-  // update state of array
-  updateArray = (post, array) => {
-    // console.log(post)
-    // console.log(this.props.posts);
-    this.setState ( prevState => {
-      console.log(prevState[array])
-      prevState[array].push(post)
-      // console.log(prevState)
-      return {
-        posts: prevState[array]
-      }
-    })
-  }
-
-  // remove a post from array
-  removeFromArray = (array, arrayIndex) => {
-
-    this.setState( prevState => {
-      console.log(prevState[array])
-      prevState[array].splice(arrayIndex, 1)
-      return {
-        [array]: prevState[array]
-      }
-    })
-  }
-
-  // edit post
-  editPost = (post, index) => {
-    // console.log(post)
-    // console.log(index)
-    // console.log(post.id)
-    console.log(JSON.stringify(post))
-    fetch(`http://localhost:3000/posts/${post.id}`, {
-      body: JSON.stringify(post),
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(updatedPost => {
-      return updatedPost.json()
-    })
-    .then(jData => {
-      // console.log(jData)
-      this.fetchPosts()
-    })
-    .catch(err => console.log(err))
-  }
-
-  // delete post
-  handleDelete = (id, arrayIndex, currentArray) => {
-    console.log(id)
-    console.log(arrayIndex)
-    fetch(`http://localhost:3000/posts/${id}`, {
-      method: 'DELETE'
-    })
-    .then(data => {
-      this.removeFromArray(currentArray, arrayIndex)
-    })
-    .catch(err => console.log(err))
-  }
-
-  componentDidMount() {
-    this.fetchPosts()
-  }
+  // // fetch all posts
+  // fetchPosts = () => {
+  //   fetch('http://localhost:3000/posts')
+  //   .then(data => data.json())
+  //   .then(jData => {
+  //     // console.log(jData)
+  //     this.setState({posts: jData})
+  //   })
+  // }
+  //
+  // // handles creating the post
+  // handleCreatePost = (post) => {
+  //   console.log(this.state.posts)
+  //   fetch('http://localhost:3000/posts', {
+  //     body: JSON.stringify(post),
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json, text/plain, */*',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   .then(createdPost => {
+  //     // console.log(createdPost);
+  //     return createdPost.json()
+  //   })
+  //   .then(jData => {
+  //     this.updateArray(jData, 'posts')
+  //     console.log(jData)
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+  //
+  // // handles updating the post
+  // handleCheck = (post, arrayIndex) => {
+  //   // console.log(post)
+  //   // console.log(arrayIndex)
+  //   // manipulate the post data
+  //   this.editPost(post, arrayIndex)
+  //
+  // }
+  //
+  // // update state of array
+  // updateArray = (post, array) => {
+  //   // console.log(post)
+  //   // console.log(this.props.posts);
+  //   this.setState ( prevState => {
+  //     console.log(prevState[array])
+  //     prevState[array].push(post)
+  //     // console.log(prevState)
+  //     return {
+  //       posts: prevState[array]
+  //     }
+  //   })
+  // }
+  //
+  // // remove a post from array
+  // removeFromArray = (array, arrayIndex) => {
+  //
+  //   this.setState( prevState => {
+  //     console.log(prevState[array])
+  //     prevState[array].splice(arrayIndex, 1)
+  //     return {
+  //       [array]: prevState[array]
+  //     }
+  //   })
+  // }
+  //
+  // // edit post
+  // editPost = (post, index) => {
+  //   console.log(JSON.stringify(post))
+  //   fetch(`http://localhost:3000/posts/${post.id}`, {
+  //     body: JSON.stringify(post),
+  //     method: 'PUT',
+  //     headers: {
+  //       'Accept': 'application/json, text/plain, */*',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   .then(updatedPost => {
+  //     return updatedPost.json()
+  //   })
+  //   .then(jData => {
+  //     // console.log(jData)
+  //     this.fetchPosts()
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+  //
+  // // delete post
+  // handleDelete = (id, arrayIndex, currentArray) => {
+  //   fetch(`http://localhost:3000/posts/${id}`, {
+  //     method: 'DELETE'
+  //   })
+  //   .then(data => {
+  //     this.removeFromArray(currentArray, arrayIndex)
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+  //
+  // componentDidMount() {
+  //   this.fetchPosts()
+  // }
   render() {
     return (
-      <div className="main-container">
-        <h1>!FB Test</h1>
-        {/*just a test for fetch users*/}
-        <button onClick={this.fetchUsers}>Get Users</button>
-        {this.state.loggedIn ?
-          <div>
-          {/*main dashboard componenets will render in Dashboard.js*/}
-            <Dashboard
-              posts={this.state.posts}
-              handleCreatePost={this.handleCreatePost}
-              handleCheck={this.handleCheck}
-              handleDelete={this.handleDelete}
-            />
-          </div>
-          :
-          <div>
-          {/*authentication page components will render in Authentication.js*/}
-            <Authentication/>
-          </div>
-        }
+      <div className="jumbotron">
+          <div className="container">
+              <div className="col-sm-8 col-sm-offset-2">
+                  <Router>
+                      <div>
+                      <h1>WELCOME TO !FB</h1>
+                          <PrivateRoute exact path="/" component={Dashboard}
+                          handleCreatePost={this.handleCreatePost}
+                          posts={this.state.posts}
+                          handleCheck={this.handleCheck}
+                           />
 
-                                <Route path="/login" component={Login} />
-                                <Route path='/register' component={NewUser}/>
-                            </div>
-                        </Router>
-                    </div>
-                </div>
-            </div>
-        );
+                          <Route path="/login" component={Login} />
+                          <Route path='/register' component={NewUser}/>
+                      </div>
+                  </Router>
+              </div>
+          </div>
+      </div>
+      );
     }
-  // render() {
-  //   return (
-  //     <div className="main-container">
-  //       <h1>!FB Test</h1>
-  //       {/*just a test for fetch users*/}
-  //       <button onClick={this.fetchUsers}>Get Users</button>
-  //       {this.state.loggedIn ?
-  //         <div>
-  //         {/*main dashboard componenets will render in Dashboard.js*/}
-  //           <Dashboard
-  //           posts={this.state.posts} handleCreatePost={this.handleCreatePost} handleCheck={this.handleCheck}/>
-  //         </div>
-  //         :
-  //         <div>
-  //         {/*authentication page components will render in Authentication.js*/}
-  //           <Authentication
-  //             loginUser={this.loginUser}
-  //           />
-  //         </div>
-  //       }
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
